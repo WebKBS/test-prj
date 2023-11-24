35d0a0d36bb0690ca60799ec2dd0b3c07ef91ca9
gangbeomsu <kbs21c1987@gmail.com>
2023-11-24 21:28:22 +0900

    first commit

---
+++ post-test/src/app/api/startInterval/route.js
@@ -0,0 +1,54 @@
+import { NextResponse } from 'next/server';
+
+let intervalId;
+
+export async function POST(req, res) {
+  try {
+    const { method } = req;
+    const body = await req.json();
+
+    if (method === 'POST') {
+      if (body.action === 'start') {
+        if (!intervalId) {
+          // setInterval 시작
+          intervalId = setInterval(() => {
+            // 실행할 코드
+            console.log('Interval is running...');
+          }, 1000); // 예시: 1초마다 실행
+
+          return NextResponse.json({
+            status: 200,
+            message: 'Interval started',
+          });
+        } else {
+          return NextResponse.json({
+            status: 400,
+            message: 'Interval already running',
+          });
+        }
+      } else if (body.action === 'stop') {
+        if (intervalId) {
+          // setInterval 멈추기
+          clearInterval(intervalId);
+          intervalId = null;
+          return NextResponse.json({
+            status: 200,
+            message: 'Interval stopped',
+          });
+        } else {
+          return NextResponse.json({
+            status: 400,
+            message: 'Interval not running',
+          });
+        }
+      } else {
+        return NextResponse.json({ status: 400, message: 'Bad request' });
+      }
+    } else {
+      return NextResponse.json({ status: 405, message: 'Method not allowed' });
+    }
+  } catch (error) {
+    console.error('Error occurred:', error);
+    return NextResponse.json({ status: 500, message: 'Internal server error' });
+  }
+}
---
+++ post-test/src/app/components/Form.jsx
@@ -0,0 +1,46 @@
+'use client';
+
+const Form = () => {
+  const handleStartInterval = async () => {
+    try {
+      const response = await fetch('/api/startInterval', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ action: 'start' }), // start 요청 보내기
+      });
+
+      const data = await response.json();
+      console.log(data.message);
+    } catch (error) {
+      console.error('Error starting interval:', error);
+    }
+  };
+
+  const handleStopInterval = async () => {
+    try {
+      const response = await fetch('/api/startInterval', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ action: 'stop' }), // stop 요청 보내기
+      });
+
+      const data = await response.json();
+      console.log(data.message);
+    } catch (error) {
+      console.error('Error stopping interval:', error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleStartInterval}>Start Interval</button>
+      <button onClick={handleStopInterval}>Stop Interval</button>
+    </div>
+  );
+};
+
+export default Form;
